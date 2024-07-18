9e7875884060b5da731eb1a8bf5eb4118c2220d8
Siba <sibaquma@gmail.com>
2024-07-18 03:09:33 +0000

    update

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,35 +9,28 @@ from typing import Callable
 
 
 def track_get_page(fn: Callable) -> Callable:
-    """Decorator for get_page"""
+    """ Decorator for get_page
+    """
     @wraps(fn)
     def wrapper(url: str) -> str:
-        """Wrapper that:
-        - Checks whether a URL's data is cached
-        - Tracks how many times get_page is called
+        """ Wrapper that:
+            - check whether a url's data is cached
+            - tracks how many times get_page is called
         """
         client = redis.Redis()
-
-        # Increment the access count for the URL
         client.incr(f'count:{url}')
-
-        # Check if the URL response is cached
-        cached_page = client.get(url)
+        cached_page = client.get(f'{url}')
         if cached_page:
             return cached_page.decode('utf-8')
-
-        # If not cached, make the HTTP request
         response = fn(url)
-
-        # Cache the response with an expiration time of 10 seconds
-        client.set(url, response, ex=10)
+        client.set(f'{url}', response, 10)
         return response
     return wrapper
 
 
 @track_get_page
 def get_page(url: str) -> str:
-    """Makes an HTTP request to a given endpoint and returns the HTML content"""
+    """ Makes a http request to a given endpoint
+    """
     response = requests.get(url)
-    response.raise_for_status()  # Ensure we raise an exception for bad responses
     return response.text
